fa4f0d9aa2e202525abe31566d58f887b296c165
Kyrylo Silin <silin@kyrylo.org>
2018-11-19 16:16:07 +0800

    Bump version

--- lib/airbrake-ruby/version.rb
+++ lib/airbrake-ruby/version.rb
@@ -2,5 +2,5 @@
 # More information: http://semver.org/
 module Airbrake
   # @return [String] the library version
-  AIRBRAKE_RUBY_VERSION = '3.0.0.rc.6'.freeze
+  AIRBRAKE_RUBY_VERSION = '3.0.0.rc.7'.freeze
 end
