f85f33cd6e49444a19dc82815f6d50437ed1dea5
Kyrylo Silin <silin@kyrylo.org>
2019-02-28 18:16:22 +0200

    Release v4.1.0

--- lib/airbrake-ruby/version.rb
+++ lib/airbrake-ruby/version.rb
@@ -2,5 +2,5 @@
 # More information: http://semver.org/
 module Airbrake
   # @return [String] the library version
-  AIRBRAKE_RUBY_VERSION = '4.0.1'.freeze
+  AIRBRAKE_RUBY_VERSION = '4.1.0'.freeze
 end
